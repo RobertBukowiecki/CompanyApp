bf60ceed0fcfd1a392789e08b9c52315a174e4f3
Robert <robert@MacBook-Pro-Robert.local>
2021-07-06 21:46:29 +0200

    add endpoint tests

---
+++ test/api/departments/delete.test.js
@@ -0,0 +1,33 @@
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../../../server.js");
+const Department = require("../../../models/department.model");
+
+chai.use(chaiHttp);
+
+const expect = chai.expect;
+const request = chai.request;
+
+describe("GET /api/departments", () => {
+  before(async () => {
+    const testDepOne = new Department({
+      _id: "5d9f1140f10a81216cfd4408",
+      name: "Department #1",
+    });
+    await testDepOne.save();
+  });
+
+  after(async () => {
+    await Department.deleteMany();
+  });
+  it("/:id should delete one department by :id ", async () => {
+    const res = await request(server).delete(
+      "/api/departments/5d9f1140f10a81216cfd4408"
+    );
+    expect(res.status).to.be.equal(200);
+    const delatedDepartment = await Department.findOne({
+      name: "Department #1",
+    });
+    expect(delatedDepartment).to.be.null;
+  });
+});
